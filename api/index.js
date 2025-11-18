88ce5da88372707f5f2cab9903ca07b1b4d19688
askarovhoziakbar <askarovhoziakbar17@gmail.com>
2025-11-19 05:35:54 +0600

    эээ

--- api/index.js
+++ api/index.js
@@ -1,2 +1,29 @@
-const server = require("./server");
-module.exports = server;
+import express from "express";
+import cors from "cors";
+import fs from "fs";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const FILE = "click.json";
+
+if (!fs.existsSync(FILE)) {
+  fs.writeFileSync(FILE, JSON.stringify({ click: 0 }));
+}
+
+app.get("/number", (req, res) => {
+  const data = JSON.parse(fs.readFileSync(FILE, "utf-8"));
+  res.json(data);
+});
+
+app.post("/number/increment", (req, res) => {
+  const data = JSON.parse(fs.readFileSync(FILE, "utf-8"));
+  data.click += 1;
+
+  fs.writeFileSync(FILE, JSON.stringify(data));
+  res.json(data);
+});
+
+export default function handler(req, res) {
+  return app(req, res);
+}
